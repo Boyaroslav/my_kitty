4eed1ccfa67aeb2fe7433de3e03ab9b555bef8cc
Boyaroslav <boyara.best@gmail.com>
2022-04-08 22:43:21 +0300

    vers 2.7.5

--- plugins/choice.py
+++ plugins/choice.py
@@ -2,6 +2,7 @@ from random import choice
 
 def answer(msg):
     ors = ["or", "или"]
+    msg = msg.rstrip("?")
     msg = msg.split()
     #  text separated by ors
     variants = []
@@ -16,10 +17,10 @@ def answer(msg):
             if i not in ors:
                 buf.append(i)
             else:
-                variants.append(''.join(buf))
+                variants.append(' '.join(buf))
                 buf.clear()
         if buf:
-            variants.append(''.join(buf))
+            variants.append(' '.join(buf))
         print(variants)
 
         return choice(variants)
