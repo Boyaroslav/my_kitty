06d9c09a33d53ea16d68916aae498b7c6e1328fe
Boyaroslav <boyara.best@gmail.com>
2022-04-08 22:31:36 +0300

    vers 2.7

---
+++ plugins/coin.py
@@ -0,0 +1,19 @@
+from random import choice
+
+
+'''
+github - https://github.com/bulochkasyablokom
+'''
+
+
+def answer(msg):
+    coin = choice(["орел", "решка"])
+
+    need = ["подбрось монетку", "орел или решка", "монетка", "орёл или решка"]
+
+    for i in need:
+        if i == msg.lower() or i in msg.lower():
+            if coin == "орел":
+                return choice([coin.capitalize(), f"Выпал {coin}."])
+            elif coin == "решка":
+                return choice([coin.capitalize(), f"Выпала {coin}."])
