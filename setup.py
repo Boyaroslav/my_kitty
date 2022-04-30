e614dc993394afe11e66f5ec4d3d8bebf344e3da
boyaroslav <boyara.best@gmail.com>
2022-04-30 12:17:25 +0300

    vers 3.6.5 setup.py updated

--- setup.py
+++ setup.py
@@ -8,13 +8,37 @@ github - https://github.com/Boyaroslav
 
 import config
 import os
-deskt = open(os.path.expanduser('~') + "/.local/share/applications/my_kitty.desktop", "w+")
+from subprocess import Popen, PIPE
+try:
+    deskt = open(os.path.expanduser('~'), "/.local/bin/my_kitty", "w+")
+    os.system("chmod +x ~/.local/bin/my_kitty")
+    
+except:
+    deskt = open("/usr/bin/my_kitty", "w+")
+    os.system("chmod +x /usr/bin/my_kitty")
+    #deskt = open(os.path.expanduser('~'), "/.local/bin/my_kitty.sh")
+try:
 
-file = ["[Desktop Entry]\n", "Name = my_kitty\n", "Version=3.6.4\n", f"Icon = {config.kitty_picture}\n",
+    deskt2 = open(os.path.expanduser('~') + "/.local/share/applications/my_kitty.desktop", "w+")
+except:
+    deskt2 = open("/usr/share/applications/my_kitty.desktop", "w+")
+
+file = ["[Desktop Entry]\n", "Name = my_kitty\n", "Version=3.6.5\n", f"Icon = {config.kitty_picture}\n",
         f"Exec = python3 {config.path}main.pyw\n", "Terminal=false\n", "Type = Application\n"]
 
-deskt.writelines(file)
+deskt2.writelines(file)
+print("file my_kitty.desktop created!")
+deskt2.close()
 
+deskt.write(f"python3 {config.path}main.pyw")
 deskt.close()
+print("file my_kitty.sh created")
+
+#p = Popen(['sudo', '-S', "echo",  '"python3',  f'{config.path}main.pyw"',  '>', '/usr/bin/my_kitty.sh'], stdin=PIPE, stderr=PIPE,universal_newlines=True)
+#sudo_prompt = p.communicate(input())[1]
+
+
+
+print("done")
 
 # now you can start my_kitty from your apps bar
